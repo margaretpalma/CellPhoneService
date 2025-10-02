9ed47aafce0408153c1acbed02fee47b520afa04
margaretpalma <mpalma1496@gmail.com>
2025-10-02 12:57:32 -0400

    initial files

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -1,15 +1,11 @@
 package com.pluralsight;
 
 public class CellPhone {
-    //private not able to be accessed outside of Cellphone.java
     private int serialNumber;
     private String model;
     private String carrier;
     private String phoneNumber;
     private String owner;
-
-    //parameterless constructor, that means theres nothing in the CellPhone()
-    //what should be done to construct a cell phone
 
     public CellPhone() {
         this.serialNumber = 0;
@@ -19,13 +15,38 @@ public class CellPhone {
         this.owner = "";
     }
 
-    public int getSerialNumber(){
-        return serialNumber;
+    public int getSerialNumber() {
+    return this.serialNumber;
+    }
+    public void setSerialNumber(int serialNumber){
+    this.serialNumber = serialNumber;
     }
 
-    public void setSerialNumber(int newSerialNumber){
-        this.serialNumber = newSerialNumber;
+    public String getModel(){
+        return this.model;
+    }
+    public void setModel(String model){
+        this.model = model;
     }
 
+    public String getCarrier(){
+        return this.carrier;
+    }
+    public void setCarrier(String carrier){
+        this.carrier = carrier;
+    }
+
+    public String getPhoneNumber(){
+        return this.phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getOwner(){
+        return this.owner;
+    }
+    public void setOwner(String owner){
+        this.owner = owner;
+    }
 }
-
--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,7 +1,41 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class CellPhoneApplication {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        CellPhone myPhone = new CellPhone();
 
+        System.out.print("What is the serial number?: ");
+        int sn = scanner.nextInt();
+        scanner.nextLine();
+
+        myPhone.setSerialNumber(sn);
+
+        System.out.print("What is the model?: ");
+        String model = scanner.nextLine();
+
+        myPhone.setModel(model);
+
+        System.out.print("What is the carrier?: ");
+        String carrier = scanner.nextLine();
+
+        myPhone.setCarrier(carrier);
+
+        System.out.print("What is the phone number?: ");
+        String phoneNumber = scanner.nextLine();
+
+        myPhone.setPhoneNumber(phoneNumber);
+
+        System.out.print("What is the owner?: ");
+        String owner = scanner.nextLine();
+        myPhone.setOwner(owner);
+
+
+        myPhone.dial("555-555-5555");
+
+        scanner.close();
     }
 }
+
